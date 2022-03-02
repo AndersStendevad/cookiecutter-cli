255a7d0484f487f56d50301c7bbed12d93de0a1a
andst <andst@itu.dk>
2022-03-02 19:23:22 +0100

    Made the working test that also testes that you can install the liberay and test the code

--- tests/test_cookiecutter.py
+++ tests/test_cookiecutter.py
@@ -1,4 +1,21 @@
+import os
+import subprocess
+from pathlib import Path
+
 def test_bake_project(cookies, cookiecutter_dict):
     result = cookies.bake(extra_context=cookiecutter_dict)
 
     assert result.exit_code == 0
+    assert result.exception is None
+
+def test_tox_in_baked_project(cookies, cookiecutter_dict):
+    path = cookies.bake(extra_context=cookiecutter_dict).project_path
+    prev_cwd = Path.cwd()
+    os.chdir(path)
+    result = subprocess.run("pip3 install -e .", shell=True, capture_output=True)
+    if result.stderr:
+        raise Exception(result.stderr.decode('utf-8'))
+    result = subprocess.run("pytest", shell=True, capture_output=True)
+    if result.stderr:
+        raise Exception(result.stderr.decode('utf-8'))
+    os.chdir(prev_cwd)
--- {{cookiecutter.package_name}}/src/{{cookiecutter.package_name}}/cli.py
+++ {{cookiecutter.package_name}}/src/{{cookiecutter.package_name}}/cli.py
@@ -1,8 +1,11 @@
+"""{{cookiecutter.package_name}} CLI"""
+
 import click
 
 @click.command()
 @click.argument("name", default="world")
 def {{cookiecutter.package_name}}(name):
+    """Hello World"""
     click.echo(f'Hello {name}!')
 
 if __name__ == '__main__':
