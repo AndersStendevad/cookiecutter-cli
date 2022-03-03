a3b39f8b4c39ec3e27ca444baa263564f7b53cc0
andst <andst@itu.dk>
2022-03-03 11:21:58 +0100

    Added working pre-commit and test to test it

--- tests/test_cookiecutter.py
+++ tests/test_cookiecutter.py
@@ -19,3 +19,15 @@ def test_tox_in_baked_project(cookies, cookiecutter_dict):
     if result.stderr:
         raise Exception(result.stderr.decode('utf-8'))
     os.chdir(prev_cwd)
+
+def test_pre_commit_in_baked_projet(cookies, cookiecutter_dict):
+    path = cookies.bake(extra_context=cookiecutter_dict).project_path
+    prev_cwd = Path.cwd()
+    os.chdir(path)
+    result = subprocess.run("pip3 install pre-commit", shell=True, capture_output=True)
+    if result.stderr:
+        raise Exception(result.stderr.decode('utf-8'))
+    result = subprocess.run("pytest", shell=True, capture_output=True)
+    if result.stderr:
+        raise Exception(result.stderr.decode('utf-8'))
+    os.chdir(prev_cwd)
