af3007d64783dfa793923ff67e37f3312ba586d9
Anders Stendevad <andst@flowtale.ai>
2022-08-30 09:59:20 +0200

    Updated tests

--- tests/test_cookiecutter.py
+++ tests/test_cookiecutter.py
@@ -1,97 +1,40 @@
-import os
-from subprocess import run, CompletedProcess
-from pathlib import Path
+""" tests for cookiecutter """
+
+import pytest
+import requests
+import socket
+
+from subprocess import run, Popen, CompletedProcess
+from time import sleep
+
+def test_bake_project(bake):
+    """check if cookiecutter will even bake"""
+    pass
 
 
-def check_result(result: CompletedProcess, prev_cwd: Path):
-    stderr = result.stderr.decode("utf-8")
-    stdout = result.stdout.decode("utf-8")
-    if result.stderr and "SetuptoolsDeprecationWarning" not in stderr:
-        os.chdir(prev_cwd)
-        raise Exception(stderr)
-    if "ERROR:" in stdout:
-        os.chdir(prev_cwd)
-        raise Exception(stdout)
-    if "FAILED" in stdout:
-        os.chdir(prev_cwd)
-        raise Exception(stdout)
-    if "WARNING" in stdout:
-        os.chdir(prev_cwd)
-        raise Exception(stdout)
+def test_pytest_in_baked_project(bake, helper):
+    """check pytest in baked project"""
+    helper.install_package(".")
+    helper.check_result(run("tox -e pytest", shell=True, capture_output=True))
 
 
-def test_bake_project(cookies, cookiecutter_dict):
-    result = cookies.bake(extra_context=cookiecutter_dict)
-    assert result.exit_code == 0
-    assert result.exception is None
-
-
-def test_pytest_in_baked_project(cookies, cookiecutter_dict):
-    path = cookies.bake(extra_context=cookiecutter_dict).project_path
-    prev_cwd = Path.cwd()
-    os.chdir(path)
-    result = run(
-        "pip install --disable-pip-version-check -e .", shell=True, capture_output=True
+def test_pre_commit_in_baked_project(bake, helper):
+    """check pre-commit in baked project"""
+    helper.check_result(
+        run("pre-commit run --all-files", shell=True, capture_output=True)
     )
-    check_result(result, prev_cwd)
-    result = run("pytest tests", shell=True, capture_output=True)
-    check_result(result, prev_cwd)
-    os.chdir(prev_cwd)
 
 
-def test_cli_in_baked_project(cookies, cookiecutter_dict):
-    path = cookies.bake(extra_context=cookiecutter_dict).project_path
-    prev_cwd = Path.cwd()
-    os.chdir(path)
-    result = run(
-        "pip install --disable-pip-version-check -e .", shell=True, capture_output=True
-    )
-    check_result(result, prev_cwd)
-    result = run(cookiecutter_dict["package_name"], shell=True, capture_output=True)
-    check_result(result, prev_cwd)
-    os.chdir(prev_cwd)
+def test_docs(bake, helper):
+    """check if docs are generated"""
+    helper.check_result(run("tox -e docs", shell=True, capture_output=True))
 
 
-def test_pre_commit_in_baked_project(cookies, cookiecutter_dict):
-    path = cookies.bake(extra_context=cookiecutter_dict).project_path
-    prev_cwd = Path.cwd()
-    os.chdir(path)
-    result = run(
-        "pip install --disable-pip-version-check pre-commit",
-        shell=True,
-        capture_output=True,
-    )
-    check_result(result, prev_cwd)
-    result = run("git init --quiet && git add .", shell=True, capture_output=True)
-    check_result(result, prev_cwd)
-    result = run("pre-commit run --all-files", shell=True, capture_output=True)
-    check_result(result, prev_cwd)
-    os.chdir(prev_cwd)
+def test_lint(bake, helper):
+    """check lint in baked project"""
+    helper.check_result(run("tox -e lint", shell=True, capture_output=True))
 
-
-def test_docs(cookies, cookiecutter_dict):
-    path = cookies.bake(extra_context=cookiecutter_dict).project_path
-    prev_cwd = Path.cwd()
-    os.chdir(path)
-    result = run(
-        "pip install --disable-pip-version-check tox", shell=True, capture_output=True
-    )
-    check_result(result, prev_cwd)
-    result = run("tox -e docs", shell=True, capture_output=True)
-    check_result(result, prev_cwd)
-    os.chdir(prev_cwd)
-
-
-def test_lint(cookies, cookiecutter_dict):
-    path = cookies.bake(extra_context=cookiecutter_dict).project_path
-    prev_cwd = Path.cwd()
-    os.chdir(path)
-    result = run(
-        "pip install --disable-pip-version-check tox", shell=True, capture_output=True
-    )
-    check_result(result, prev_cwd)
-    result = run("git init --quiet && git add .", shell=True, capture_output=True)
-    check_result(result, prev_cwd)
-    result = run("tox -e lint", shell=True, capture_output=True)
-    check_result(result, prev_cwd)
-    os.chdir(prev_cwd)
+def test_cli(bake, helper):
+    """check cli in baked project"""
+    helper.check_result(run("pip install -e .", shell=True, capture_output=True))
+    helper.check_result(run("package --help", shell=True, capture_output=True))
